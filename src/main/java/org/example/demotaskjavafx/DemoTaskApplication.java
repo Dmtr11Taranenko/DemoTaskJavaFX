f1832ca94fda02b1727c19bc89da01601b999f58
dtaranenko <dtaranenko@nntc.pro>
2025-06-05 01:40:08 +0700

    Created the layout of the main window using the SceneBuilder

--- src/main/java/org/example/demotaskjavafx/DemoTaskApplication.java
+++ src/main/java/org/example/demotaskjavafx/DemoTaskApplication.java
@@ -1,38 +1,33 @@
+/**
+ * В данной программе реализовано техническое задание: "Разработка JavaFX-приложения для работы с числами"
+ * @version 1.0 05.06.2025
+ * @author Taranenko Dmitrii
+ */
+
 package org.example.demotaskjavafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
 
-public class HelloApplication extends Application {
-//    @Override
-//    public void start(Stage stage) throws IOException {
-//        URL fxmlUrl = HelloApplication.class.getResource("/org/example/demotaskjavafx/hello-view.fxml");
-//        FXMLLoader fxmlLoader = new FXMLLoader(fxmlUrl);
-//        Scene scene = new Scene(fxmlLoader.load(), 700, 700);
-//        stage.setTitle("DemoTaskJavaFX");
-//        stage.setScene(scene);
-//        stage.show();
-//    }
+public class DemoTaskApplication extends Application {
 
+    // Создание среды приложения
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
         Scene scene = new Scene(root, 600, 400);
         stage.setTitle("DemoTaskJavaFX");
-
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
+    // Запуск приложения
     public static void main(String[] args) {
         launch();
     }
